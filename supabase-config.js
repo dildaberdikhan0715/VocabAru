a21d18ae67c552d861190f4adc524a051357082d
dildaberdikhan0715 <berdikhandilda@gmail.com>
2026-08-20 16:46:19 +0500

    Update Supabase anon key in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 window.VOCABARU_SUPABASE_URL = "https://lomjjuiddbwwsygnsapx.supabase.co";
-window.VOCABARU_SUPABASE_ANON_KEY = "";
+window.VOCABARU_SUPABASE_ANON_KEY = "sb_publishable_9TAhhfkNqos3zD6J0Po5tA_DvZDUip4";
